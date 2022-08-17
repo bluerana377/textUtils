c849f0f6c7011f556a0192a911aff7e2f71a716d
bluerana377 <104701571+bluerana377@users.noreply.github.com>
2022-08-17 16:53:20 -0700

    Add files via upload

---
+++ build/precache-manifest.216a3a9e105881bd1a6b4875644a0547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae892ea4bcd2e77df151",
+    "url": "/static/css/main.cd079626.chunk.css"
+  },
+  {
+    "revision": "ae892ea4bcd2e77df151",
+    "url": "/static/js/main.ae892ea4.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "0a0a66a0ab074fa83a22",
+    "url": "/static/js/2.0a0a66a0.chunk.js"
+  },
+  {
+    "revision": "769c94daf783692f2c65",
+    "url": "/static/js/runtime~main.769c94da.js"
+  },
+  {
+    "revision": "b8f23c2a35b9668d4da81880bb4cd84a",
+    "url": "/index.html"
+  }
+];
